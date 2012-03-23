0280364bb2b0b0f49d72c1dc6402f08151c09b94
Andrea Arcangeli <aarcange@redhat.com>
2012-03-23 20:35:07 +0100

    autonuma: CPU follows memory algorithm
    This algorithm takes as input the statistical information filled by the
    knuma_scand (mm->mm_autonuma) and by the NUMA hinting page faults
    (p->task_autonuma), evaluates it for the current scheduled task, and
    compares it against every other running process to see if it should
    move the current task to another NUMA node.
    
    When the scheduler decides if the task should be migrated to a
    different NUMA node or to stay in the same NUMA node, the decision is
    then stored into p->task_autonuma->task_selected_nid. The fair
    scheduler then tries to keep the task on the task_selected_nid.
    
    Code include fixes and cleanups from Hillf Danton <dhillf@gmail.com>.

---
+++ include/linux/autonuma_sched.h
@@ -0,0 +1,59 @@
+#ifndef _LINUX_AUTONUMA_SCHED_H
+#define _LINUX_AUTONUMA_SCHED_H
+
+#include <linux/autonuma_flags.h>
+
+#ifdef CONFIG_AUTONUMA
+
+extern void __sched_autonuma_balance(void);
+extern bool sched_autonuma_can_migrate_task(struct task_struct *p,
+					    int strict_numa, int dst_cpu,
+					    enum cpu_idle_type idle);
+
+/*
+ * Return true if the specified CPU is in this task's selected_nid (or
+ * there is no affinity set for the task).
+ */
+static bool inline task_autonuma_cpu(struct task_struct *p, int cpu)
+{
+	int task_selected_nid;
+	struct task_autonuma *task_autonuma = p->task_autonuma;
+
+	if (!task_autonuma)
+		return true;
+
+	task_selected_nid = ACCESS_ONCE(task_autonuma->task_selected_nid);
+	if (task_selected_nid < 0 || task_selected_nid == cpu_to_node(cpu))
+		return true;
+	else
+		return false;
+}
+
+static inline void sched_autonuma_balance(void)
+{
+	struct task_autonuma *ta = current->task_autonuma;
+
+	if (ta && current->mm)
+		__sched_autonuma_balance();
+}
+
+#else /* CONFIG_AUTONUMA */
+
+static inline bool sched_autonuma_can_migrate_task(struct task_struct *p,
+						   int strict_numa,
+						   int dst_cpu,
+						   enum cpu_idle_type idle)
+{
+	return true;
+}
+
+static bool inline task_autonuma_cpu(struct task_struct *p, int cpu)
+{
+	return true;
+}
+
+static inline void sched_autonuma_balance(void) {}
+
+#endif /* CONFIG_AUTONUMA */
+
+#endif /* _LINUX_AUTONUMA_SCHED_H */
