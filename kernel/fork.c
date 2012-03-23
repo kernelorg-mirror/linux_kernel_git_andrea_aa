b174c47332d96795ddb50e52899389a70fecdc94
Andrea Arcangeli <aarcange@redhat.com>
2012-03-23 21:10:54 +0100

    autonuma: autonuma_enter/exit
    The first gear in the whole AutoNUMA algorithm is knuma_scand. If
    knuma_scand doesn't run AutoNUMA is a full bypass. If knuma_scand is
    stopped, soon all other AutoNUMA gears will settle down too.
    
    knuma_scand is the daemon that sets the pmd_numa and pte_numa and
    allows the NUMA hinting page faults to start and then all other
    actions follows as a reaction to that.
    
    knuma_scand scans a list of "mm" and this is where we register and
    unregister the "mm" into AutoNUMA for knuma_scand to scan them.

--- kernel/fork.c
+++ kernel/fork.c
@@ -67,6 +67,7 @@
 #include <linux/oom.h>
 #include <linux/khugepaged.h>
 #include <linux/signalfd.h>
+#include <linux/autonuma.h>
 
 #include <asm/pgtable.h>
 #include <asm/pgalloc.h>
@@ -505,6 +506,7 @@ static struct mm_struct *mm_init(struct mm_struct *mm, struct task_struct *p)
 	if (likely(!mm_alloc_pgd(mm))) {
 		mm->def_flags = 0;
 		mmu_notifier_mm_init(mm);
+		autonuma_enter(mm);
 		return mm;
 	}
 
@@ -572,6 +574,7 @@ void mmput(struct mm_struct *mm)
 		exit_aio(mm);
 		ksm_exit(mm);
 		khugepaged_exit(mm); /* must run before exit_mmap */
+		autonuma_exit(mm); /* must run before exit_mmap */
 		exit_mmap(mm);
 		set_mm_exe_file(mm, NULL);
 		if (!list_empty(&mm->mmlist)) {
