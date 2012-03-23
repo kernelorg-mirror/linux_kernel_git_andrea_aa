46012a653e2b3821769b5c94bd306533acfed74e
Andrea Arcangeli <aarcange@redhat.com>
2012-03-23 21:59:32 +0100

    autonuma: default mempolicy follow AutoNUMA
    If an task_selected_nid has already been selected for the task, try to
    allocate memory from it even if it's temporarily not the local
    node. Chances are it's where most of its memory is already located and
    where it will run in the future.
    
    Acked-by: Rik van Riel <riel@redhat.com>

--- mm/mempolicy.c
+++ mm/mempolicy.c
@@ -1951,10 +1951,18 @@ retry_cpuset:
 	 */
 	if (pol->mode == MPOL_INTERLEAVE)
 		page = alloc_page_interleave(gfp, order, interleave_nodes(pol));
-	else
+	else {
+		int nid = -1;
+#ifdef CONFIG_AUTONUMA
+		if (current->task_autonuma)
+			nid = current->task_autonuma->task_selected_nid;
+#endif
+		if (nid < 0)
+			nid = numa_node_id();
 		page = __alloc_pages_nodemask(gfp, order,
-				policy_zonelist(gfp, pol, numa_node_id()),
+				policy_zonelist(gfp, pol, nid),
 				policy_nodemask(gfp, pol));
+	}
 
 	if (unlikely(!put_mems_allowed(cpuset_mems_cookie) && !page))
 		goto retry_cpuset;
