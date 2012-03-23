dcc6b6294fd1f2f58a3b375fa9e2bf0dc7fd3c98
Andrea Arcangeli <aarcange@redhat.com>
2012-03-23 20:35:07 +0100

    autonuma: CPU follows memory algorithm
    This algorithm takes as input the statistical information filled by the
    knuma_scand (mm->mm_autonuma) and by the NUMA hinting page faults
    (p->task_autonuma), evaluates it for the current scheduled task, and
    compares it against every other running process to see if it should
    move the current task to another NUMA node.
    
    When the scheduler decides if the task should be migrated to a
    different NUMA node or to stay in the same NUMA node, the decision is
    then stored into p->task_autonuma->task_selected_nid. The fair
    scheduler then tries to keep the task on the task_selected_nid.
    
    Code include fixes and cleanups from Hillf Danton <dhillf@gmail.com>.

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1523,6 +1523,9 @@ struct task_struct {
 	struct mempolicy *mempolicy;	/* Protected by alloc_lock */
 	short il_next;
 	short pref_node_fork;
+#ifdef CONFIG_AUTONUMA
+	struct task_autonuma *task_autonuma;
+#endif
 #endif
 	struct rcu_head rcu;
 
