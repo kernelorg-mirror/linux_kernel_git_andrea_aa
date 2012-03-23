3ba53c4ae85e7835f188f30563ef125b6e65c7eb
Andrea Arcangeli <aarcange@redhat.com>
2012-03-23 22:02:58 +0100

    autonuma: make khugepaged pte_numa aware
    If any of the ptes that khugepaged is collapsing was a pte_numa, the
    resulting trans huge pmd will be a pmd_numa too.
    
    See the comment inline for why we require just one pte_numa pte to
    make a pmd_numa pmd. If needed later we could change the number of
    pte_numa ptes required to create a pmd_numa and make it tunable with
    sysfs too.

--- mm/huge_memory.c
+++ mm/huge_memory.c
@@ -1816,12 +1816,13 @@ out:
 	return isolated;
 }
 
-static void __collapse_huge_page_copy(pte_t *pte, struct page *page,
+static bool __collapse_huge_page_copy(pte_t *pte, struct page *page,
 				      struct vm_area_struct *vma,
 				      unsigned long address,
 				      spinlock_t *ptl)
 {
 	pte_t *_pte;
+	bool mknuma = false;
 	for (_pte = pte; _pte < pte+HPAGE_PMD_NR; _pte++) {
 		pte_t pteval = *_pte;
 		struct page *src_page;
@@ -1849,11 +1850,24 @@ static void __collapse_huge_page_copy(pte_t *pte, struct page *page,
 			page_remove_rmap(src_page);
 			spin_unlock(ptl);
 			free_page_and_swap_cache(src_page);
+
+			/*
+			 * If just one pte_numa should be enough to
+			 * create a pmd_numa is unclear, but it's
+			 * likely better to overstimate the NUMA node
+			 * affinity to a node where we just collapsed
+			 * an hugepage, rather than understimate
+			 * it. If AUTONUMA_SCAN_PMD_FLAG is set it
+			 * shouldn't make a difference anyway.
+			 */
+			mknuma |= pte_numa(pteval);
 		}
 
 		address += PAGE_SIZE;
 		page++;
 	}
+
+	return mknuma;
 }
 
 static void collapse_huge_page(struct mm_struct *mm,
@@ -1871,6 +1885,7 @@ static void collapse_huge_page(struct mm_struct *mm,
 	spinlock_t *ptl;
 	int isolated;
 	unsigned long hstart, hend;
+	bool mknuma = false;
 
 	VM_BUG_ON(address & ~HPAGE_PMD_MASK);
 #ifndef CONFIG_NUMA
@@ -1989,7 +2004,8 @@ static void collapse_huge_page(struct mm_struct *mm,
 	 */
 	anon_vma_unlock(vma->anon_vma);
 
-	__collapse_huge_page_copy(pte, new_page, vma, address, ptl);
+	mknuma = pmd_numa(_pmd);
+	mknuma |= __collapse_huge_page_copy(pte, new_page, vma, address, ptl);
 	pte_unmap(pte);
 	__SetPageUptodate(new_page);
 	pgtable = pmd_pgtable(_pmd);
@@ -1999,6 +2015,8 @@ static void collapse_huge_page(struct mm_struct *mm,
 	_pmd = mk_pmd(new_page, vma->vm_page_prot);
 	_pmd = maybe_pmd_mkwrite(pmd_mkdirty(_pmd), vma);
 	_pmd = pmd_mkhuge(_pmd);
+	if (mknuma)
+		_pmd = pmd_mknuma(_pmd);
 
 	/*
 	 * spin_lock() below is not the equivalent of smp_wmb(), so
