d30308eb9c0cefe325f05a531b3d580222afd628
Andrea Arcangeli <aarcange@redhat.com>
2012-03-23 20:51:55 +0100

    autonuma: add page structure fields
    On 64bit archs, 20 bytes are used for async memory migration (specific
    to the knuma_migrated per-node threads), and 4 bytes are used for the
    thread NUMA false sharing detection logic.
    
    This is a bad implementation due lack of time to do a proper one.
    
    These AutoNUMA new fields must be moved to the pgdat like memcg
    does. So that they're only allocated at boot time if the kernel is
    booted on NUMA hardware. And so that they're not allocated even if
    booted on NUMA hardware if "noautonuma" is passed as boot parameter to
    the kernel.

--- include/linux/mm_types.h
+++ include/linux/mm_types.h
@@ -125,6 +125,31 @@ struct page {
 		struct page *first_page;	/* Compound tail pages */
 	};
 
+#ifdef CONFIG_AUTONUMA
+	/*
+	 * FIXME: move to pgdat section along with the memcg and allocate
+	 * at runtime only in presence of a numa system.
+	 */
+	/*
+	 * To modify autonuma_last_nid lockless the architecture,
+	 * needs SMP atomic granularity < sizeof(long), not all archs
+	 * have that, notably some alpha. Archs without that requires
+	 * autonuma_last_nid to be a long.
+	 */
+#if BITS_PER_LONG > 32
+	int autonuma_migrate_nid;
+	int autonuma_last_nid;
+#else
+#if MAX_NUMNODES >= 32768
+#error "too many nodes"
+#endif
+	/* FIXME: remember to check the updates are atomic */
+	short autonuma_migrate_nid;
+	short autonuma_last_nid;
+#endif
+	struct list_head autonuma_migrate_node;
+#endif
+
 	/*
 	 * On machines where all RAM is mapped into kernel address space,
 	 * we can simply calculate the virtual address. On machines with
