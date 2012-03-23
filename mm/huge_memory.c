d774fc666207b9c1c65231e3fd282ca68e6f0ae8
Andrea Arcangeli <aarcange@redhat.com>
2012-03-23 22:01:31 +0100

    autonuma: call autonuma_split_huge_page()
    This is needed to make sure the tail pages are also queued into the
    migration queues of knuma_migrated across a transparent hugepage
    split.
    
    Acked-by: Rik van Riel <riel@redhat.com>

--- mm/huge_memory.c
+++ mm/huge_memory.c
@@ -17,6 +17,7 @@
 #include <linux/khugepaged.h>
 #include <linux/freezer.h>
 #include <linux/mman.h>
+#include <linux/autonuma.h>
 #include <asm/tlb.h>
 #include <asm/pgalloc.h>
 #include "internal.h"
@@ -1316,6 +1317,7 @@ static void __split_huge_page_refcount(struct page *page)
 		BUG_ON(!PageSwapBacked(page_tail));
 
 		lru_add_page_tail(page, page_tail, lruvec);
+		autonuma_migrate_split_huge_page(page, page_tail);
 	}
 	atomic_sub(tail_count, &page->_count);
 	BUG_ON(__page_count(page) <= 0);
