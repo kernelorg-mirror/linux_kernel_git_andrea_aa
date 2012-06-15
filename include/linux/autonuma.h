6567b4ffa4f7567fdb539224cd9acc6012748c74
Andrea Arcangeli <aarcange@redhat.com>
2012-06-15 19:27:01 +0200

    autonuma: bugcheck page_autonuma fields on newly allocated pages
    Debug tweak.

--- include/linux/autonuma.h
+++ include/linux/autonuma.h
@@ -29,6 +29,24 @@ static inline void autonuma_free_page(struct page *page)
 	}
 }
 
+static inline int autonuma_check_new_page(struct page *page)
+{
+	struct page_autonuma *page_autonuma;
+	int ret = 0;
+	if (autonuma_possible()) {
+		page_autonuma = lookup_page_autonuma(page);
+		if (unlikely(page_autonuma->autonuma_migrate_nid != -1)) {
+			ret = 1;
+			WARN_ON(1);
+		}
+		if (unlikely(page_autonuma->autonuma_last_nid != -1)) {
+			ret = 1;
+			WARN_ON(1);
+		}
+	}
+	return ret;
+}
+
 #define autonuma_printk(format, args...) \
 	if (autonuma_debug()) printk(format, ##args)
 
@@ -41,6 +59,7 @@ static inline void autonuma_migrate_split_huge_page(struct page *page,
 						    struct page *page_tail) {}
 static inline void autonuma_setup_new_exec(struct task_struct *p) {}
 static inline void autonuma_free_page(struct page *page) {}
+static inline int autonuma_check_new_page(struct page *page) { return 0; }
 
 #endif /* CONFIG_AUTONUMA */
 
