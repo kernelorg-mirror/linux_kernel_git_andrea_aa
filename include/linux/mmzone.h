159f4a4584d31af0fcb366b64550b3205a2a1e1b
Andrea Arcangeli <aarcange@redhat.com>
2012-06-19 18:55:25 +0200

    autonuma: shrink the per-page page_autonuma struct size
    From 32 to 12 bytes, so the AutoNUMA memory footprint is reduced to
    0.29% of RAM.
    
    This however will fail to migrate pages above a 16 Terabyte offset
    from the start of each node (migration failure isn't fatal, simply
    those pages will not follow the CPU, a warning will be printed in the
    log just once in that case).
    
    AutoNUMA will also fail to build if there are more than (2**15)-1
    nodes supported by the MAX_NUMNODES at build time (it would be easy to
    relax it to (2**16)-1 nodes without increasing the memory footprint,
    but it's not even worth it, so let's keep the negative space reserved
    for now).
    
    This means the max RAM configuration fully supported by AutoNUMA
    becomes AUTONUMA_LIST_MAX_PFN_OFFSET multiplied by 32767 nodes
    multiplied by the PAGE_SIZE (assume 4096 here, but for some archs it's
    bigger).
    
    4096*32767*(0xffffffff-3)>>(10*5) = 511 PetaBytes.

--- include/linux/mmzone.h
+++ include/linux/mmzone.h
@@ -17,6 +17,7 @@
 #include <linux/pageblock-flags.h>
 #include <generated/bounds.h>
 #include <linux/atomic.h>
+#include <linux/autonuma_list.h>
 #include <asm/page.h>
 
 /* Free memory management - zoned buddy allocator.  */
@@ -733,7 +734,7 @@ typedef struct pglist_data {
 	 * <linux/page_autonuma.h> and the below field must remain the
 	 * last one of this structure.
 	 */
-	struct list_head autonuma_migrate_head[0];
+	struct autonuma_list_head autonuma_migrate_head[0];
 #endif
 	/* do not add more variables here, the above array size is dynamic */
 } pg_data_t;
