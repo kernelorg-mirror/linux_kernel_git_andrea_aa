fa5fd626820c4b0bcb867618716ba931c4699b1f
Andrea Arcangeli <aarcange@redhat.com>
2013-05-27 23:21:25 +0200

    mm: zone_reclaim: compaction: don't depend on kswapd to invoke reset_isolation_suitable
    If kswapd never need to run (only __GFP_NO_KSWAPD allocations and
    plenty of free memory) compaction is otherwise crippled down and stops
    running for a while after the free/isolation cursor meets. After that
    allocation can fail for a full cycle of compaction_deferred, until
    compaction_restarting finally reset it again.
    
    Stopping compaction for a full cycle after the cursor meets, even if
    it never failed and it's not going to fail, doesn't make sense.
    
    We already throttle compaction CPU utilization using
    defer_compaction. We shouldn't prevent compaction to run after each
    pass completes when the cursor meets, unless it failed.
    
    This makes direct compaction functional again. The throttling of
    direct compaction is still controlled by the defer_compaction
    logic.
    
    kswapd still won't risk to reset compaction, and it will wait direct
    compaction to do so. Not sure if this is ideal but it at least
    decreases the risk of kswapd doing too much work. kswapd will only run
    one pass of compaction until some allocation invokes compaction again.
    
    This decreased reliability of compaction was introduced in commit
    62997027ca5b3d4618198ed8b1aba40b61b1137b .
    
    Signed-off-by: Andrea Arcangeli <aarcange@redhat.com>
    Reviewed-by: Rik van Riel <riel@redhat.com>
    Acked-by: Rafael Aquini <aquini@redhat.com>
    Acked-by: Mel Gorman <mgorman@suse.de>

--- mm/compaction.c
+++ mm/compaction.c
@@ -221,7 +221,6 @@ static void __reset_isolation_suitable(struct zone *zone)
 	zone->compact_cached_migrate_pfn[0] = start_pfn;
 	zone->compact_cached_migrate_pfn[1] = start_pfn;
 	zone->compact_cached_free_pfn = end_pfn;
-	zone->compact_blockskip_flush = false;
 
 	/* Walk the zone and mark every pageblock as suitable for isolation */
 	for (pfn = start_pfn; pfn < end_pfn; pfn += pageblock_nr_pages) {
@@ -240,7 +239,7 @@ static void __reset_isolation_suitable(struct zone *zone)
 	}
 }
 
-void reset_isolation_suitable(pg_data_t *pgdat)
+static void reset_isolation_suitable(pg_data_t *pgdat)
 {
 	int zoneid;
 
@@ -250,8 +249,7 @@ void reset_isolation_suitable(pg_data_t *pgdat)
 			continue;
 
 		/* Only flush if a full compaction finished recently */
-		if (zone->compact_blockskip_flush)
-			__reset_isolation_suitable(zone);
+		__reset_isolation_suitable(zone);
 	}
 }
 
@@ -1154,13 +1152,12 @@ static int __compact_finished(struct zone *zone, struct compact_control *cc,
 		zone->compact_cached_free_pfn = zone_end_pfn(zone);
 
 		/*
-		 * Mark that the PG_migrate_skip information should be cleared
-		 * by kswapd when it goes to sleep. kswapd does not set the
-		 * flag itself as the decision to be clear should be directly
-		 * based on an allocation request.
+		 * Clear the PG_migrate_skip information. kswapd does
+		 * not clear it as the decision to be clear should be
+		 * directly based on an allocation request.
 		 */
 		if (!current_is_kswapd())
-			zone->compact_blockskip_flush = true;
+			__reset_isolation_suitable(zone);
 
 		return COMPACT_COMPLETE;
 	}
