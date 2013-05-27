cfce62e64fcf7df29a2f4ba90886f52b77b1fd56
Andrea Arcangeli <aarcange@redhat.com>
2013-05-27 23:13:41 +0200

    mm: zone_reclaim: compaction: scan all memory with /proc/sys/vm/compact_memory
    Reset the stats so /proc/sys/vm/compact_memory will scan all memory.
    
    Signed-off-by: Andrea Arcangeli <aarcange@redhat.com>
    Reviewed-by: Rik van Riel <riel@redhat.com>
    Acked-by: Rafael Aquini <aquini@redhat.com>
    Acked-by: Mel Gorman <mgorman@suse.de>

--- mm/compaction.c
+++ mm/compaction.c
@@ -1643,13 +1643,15 @@ void compact_pgdat(pg_data_t *pgdat, int order)
 
 static void compact_node(int nid)
 {
+	pg_data_t *pgdat = NODE_DATA(nid);
 	struct compact_control cc = {
 		.order = -1,
 		.mode = MIGRATE_SYNC,
 		.ignore_skip_hint = true,
 	};
 
-	__compact_pgdat(NODE_DATA(nid), &cc);
+	reset_isolation_suitable(pgdat);
+	__compact_pgdat(pgdat, &cc);
 }
 
 /* Compact all nodes in the system */
